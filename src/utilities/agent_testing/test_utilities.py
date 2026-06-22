ad229f7e9d82747dcf8efd000a412118f5d291e2
AdiJoost <82035212+AdiJoost@users.noreply.github.com>
2026-06-22 17:16:44 +0200

    Add files via upload

--- src/utilities/agent_testing/test_utilities.py
+++ src/utilities/agent_testing/test_utilities.py
@@ -7,12 +7,12 @@ from typing import List
 from mcp.types import CallToolResult
 
 from src.model.ait.test_case.reduced_test_case_dto import ReducedTestCaseDTO
-from src.model.ait.test_case.reduced_test_section_dto import \
-    ReducedTestSectionDTO
+from src.model.ait.test_case.reduced_test_section_dto import ReducedTestSectionDTO
 from src.model.ait.test_case.test_case_dto import TestCaseDTO
 from src.model.ait.test_case.test_section_dto import TestSectionDTO
 from src.model.prompting.prompt_candidate import PromptCandidate
 from src.model.prompting.prompting_coalition import PromptingCoalition
+from src.model.result.enums.loop_stop_reason import LoopStopReason
 from src.model.result.metrics.posterior_dto import PosteriorMetrics
 from src.model.result.result import ResultDTO
 from src.model.result.test_stats import TestStats
@@ -43,7 +43,12 @@ def getPromptingMatrix(
 
 
 def createOrExtendResult(
-    results: TestStats, coalition: PromptingCoalition, resultpath: Path
+    results: TestStats,
+    coalition: PromptingCoalition,
+    resultpath: Path,
+    executionTime: int,
+    numberOfRounds: int,
+    loopStopReason: LoopStopReason,
 ):
     if not os.path.exists(resultpath):
         result = ResultDTO(prior=results.prior, results=[])
@@ -52,10 +57,17 @@ def createOrExtendResult(
             file.write(result.model_dump_json(indent=2))
     with open(resultpath, "r") as file:
         result = ResultDTO.model_validate_json(file.read())
-    updatedResult = PosteriorMetrics(metrics=results.posterior, coalition=coalition)
+    updatedResult = PosteriorMetrics(
+        metrics=results.posterior,
+        coalition=coalition,
+        executionTime=executionTime,
+        numberOfRounds=numberOfRounds,
+        loopStopReason=loopStopReason,
+    )
     result.results.append(updatedResult)
     with open(resultpath, "w") as file:
         file.write(result.model_dump_json(indent=2))
+
 
 def getReducedTestcaseFromCall(message: CallToolResult) -> ReducedTestCaseDTO:
     if message and len(message.content) > 0:
@@ -63,15 +75,18 @@ def getReducedTestcaseFromCall(message: CallToolResult) -> ReducedTestCaseDTO:
         return _reduceTestcase(testCaseDTO=testCaseDTO)
     return "TestCaseDTO not found. Use the MCP to get the current testcase"
 
+
 def _reduceTestcase(testCaseDTO: TestCaseDTO) -> str:
     testSectionDTOList = []
     for testSectionDTO in testCaseDTO.testSectionDTOList:
         newTestSection = ReducedTestSectionDTO(
             title=testSectionDTO.title,
             titleType=testSectionDTO.titleType,
-            dbId=testSectionDTO.dbId
+            dbId=testSectionDTO.dbId,
         )
         testSectionDTOList.append(newTestSection)
-    
-    reducedTestCaseDTO = ReducedTestCaseDTO(testSectionDTOList=testSectionDTOList, title=testCaseDTO.title)
+
+    reducedTestCaseDTO = ReducedTestCaseDTO(
+        testSectionDTOList=testSectionDTOList, title=testCaseDTO.title
+    )
     return reducedTestCaseDTO.model_dump_json(indent=2)
